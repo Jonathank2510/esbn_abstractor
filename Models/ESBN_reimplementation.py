Fixed first ESBN by correcting softmax dimension

Models/ESBN_reimplementation.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras.layers import LSTM, Dense
-from modules import *
+from Models.modules import Encoder_conv
+from Models.ESBNLayer import ESBNLayer
 
 #recreate distribution of three in tensorflow
 # TODO: implement temporal context norm, use @tf.function decorator
@@ -16,15 +17,15 @@ class ESBN(tf.keras.Model):
         self.key_size = 256
         self.hidden_size = 512
         self.lstm = LSTM(self.hidden_size, return_state=True)
-        self.key_w_out = Dense(self.key_size)
+        self.key_w_out = Dense(self.key_size, activation="relu")
         self.g_out = Dense(1, activation="sigmoid")
         self.confidence_gain = tf.Variable(1, dtype=tf.float32)
         self.confidence_bias = tf.Variable(0, dtype=tf.float32)
         self.y_out = Dense(y_dim, activation="softmax")
 
         # Context normalization parameters
-        self.gamma = tf.Variable(tf.ones(self.z_size))
-        self.beta = tf.Variable(tf.zeros(self.z_size))
+        self.gamma = tf.Variable(tf.ones(self.z_size), name="tcn_gamma")
+        self.beta = tf.Variable(tf.zeros(self.z_size), name="tcn_beta")
 
 
     #@tf.function
@@ -68,7 +69,8 @@ class ESBN(tf.keras.Model):
                 key_r = tf.zeros((x_seq.shape[0], 1, self.key_size + 1))
             else:
                 # Read key
-                w_k = tf.keras.activations.softmax(tf.reduce_sum((z_t * M_v), 2, keepdims=True))
+                similarity = tf.reduce_sum((z_t * M_v), 2, keepdims=True)
+                w_k = tf.keras.activations.softmax(similarity, axis=1)
                 c_k = tf.keras.activations.sigmoid(tf.reduce_sum((z_t * M_v), 2, keepdims=True) * self.confidence_gain + self.confidence_bias)
                 weighted_sum = tf.reduce_sum(w_k * tf.concat([M_k, c_k], axis=2), axis=1, keepdims=True)
                 key_r = g * weighted_sum
@@ -81,13 +83,70 @@ class ESBN(tf.keras.Model):
                 M_k = tf.concat([M_k, key_w], 1)
                 M_v = tf.concat([M_v, z_t], 1)
 
-        return y_pred_linear, y_pred
+            if t == x_seq.shape[1]:
+                self.last_M_k = M_k
+                self.last_M_v = M_v
+            if t == x_seq.shape[1] - 1:
+                self.second_last_sim = similarity
+                self.secon_last_w = w_k
+
+        return y_pred_linear
     
     def apply_context_norm(self, z_seq):
-                eps = 1e-8
-                z_mu = tf.reduce_mean(z_seq, 1, keepdims=True)
-                z_sigma = tf.math.sqrt(tf.math.reduce_variance(z_seq, 1, keepdims=True) + eps)
-                z_seq = (z_seq - z_mu) / z_sigma
-                z_seq = (z_seq * self.gamma) + self.beta
-                return z_seq
-    
+        eps = 1e-8
+        z_mu = tf.reduce_mean(z_seq, 1, keepdims=True)
+        z_sigma = tf.math.sqrt(tf.math.reduce_variance(z_seq, 1, keepdims=True) + eps)
+        z_seq = (z_seq - z_mu) / z_sigma
+        z_seq = (z_seq * self.gamma) + self.beta
+        return z_seq
+
+
+
+
+
+class Model2(tf.keras.Model):
+    def __init__(self, y_dim):
+        super(Model2, self).__init__()
+        # Encoder
+        self.encoder = Encoder_conv()
+
+        # ESBN parameters
+        self.z_size = 128
+        self.key_size = 256
+        self.hidden_size = 512
+
+        self.esbn_layer = ESBNLayer(self.key_size, self.hidden_size)
+        self.out = Dense(y_dim, activation="softmax")
+
+        # Context normalization parameters
+        self.gamma = tf.Variable(tf.ones(self.z_size))
+        self.beta = tf.Variable(tf.zeros(self.z_size))
+
+
+    def call(self, x_seq):
+        # TODO: make code more tf-style
+        #encode all images in sequence
+        z_seq = []
+        for t in range(x_seq.shape[1]):
+            x_t = tf.expand_dims(x_seq[:,t,:,:], 1)
+            z_t = self.encoder(x_t)
+            z_seq.append(z_t)
+        # Apply temporal context normalization
+        z_seq = self.apply_context_norm(z_seq)
+        #stack embeddings along the first dimension (timesteps)
+        z_seq = tf.stack(z_seq, 1)
+        # Append zero-element to input sequence to obtain predition from ESBN
+        z_seq = tf.concat([z_seq, tf.zeros((z_seq.shape[0], 1, z_seq.shape[2]))], 1)
+        # Get (purely relational processed) predictions from ESBN layer
+        esbn_out = self.esbn_layer(z_seq)
+        logits = self.out(esbn_out)
+        return logits
+        
+
+    def apply_context_norm(self, z_seq):
+            eps = 1e-8
+            z_mu = tf.reduce_mean(z_seq, 1, keepdims=True)
+            z_sigma = tf.math.sqrt(tf.math.reduce_variance(z_seq, 1, keepdims=True) + eps)
+            z_seq = (z_seq - z_mu) / z_sigma
+            z_seq = (z_seq * self.gamma) + self.beta
+            return z_seq

Utils/train_and_eval.py
@@ -11,7 +11,7 @@ test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('test_accuracy')
 # Perform one epoch of training
 def train_step(model, x_seq, targets, optimizer, loss_fn):
 	with tf.GradientTape() as tape:
-		y_pred_linear, y_pred = model(x_seq)
+		y_pred_linear = model(x_seq)
 		loss = loss_fn(targets, y_pred_linear)
 	grads = tape.gradient(loss, model.trainable_variables)
 	optimizer.apply_gradients(zip(grads, model.trainable_variables))
@@ -23,7 +23,7 @@ def train_step(model, x_seq, targets, optimizer, loss_fn):
 def test_step(model, all_imgs, test_loader, loss_fn):
 	for seq_ind, y in test_loader:
 		x_seq = tf.gather(all_imgs, seq_ind)
-		y_pred_linear, y_pred = model(x_seq)
+		y_pred_linear = model(x_seq)
 		loss = loss_fn(y, y_pred_linear)
 		test_loss(loss)
 		test_accuracy(y, y_pred_linear)